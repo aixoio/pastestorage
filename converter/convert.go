bd664789989a56c122ec9c692f01d1c9cc1d1160
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-24 19:26:21 +1000

    add basic spliting code

--- converter/convert.go
+++ converter/convert.go
@@ -2,6 +2,7 @@ package converter
 
 import (
 	"encoding/base64"
+	"math"
 	"os"
 )
 
@@ -15,5 +16,22 @@ func ConvertFileToText(filename string) ([]string, error) {
 
 	unsplited := base64.StdEncoding.EncodeToString(file_dat)
 
-	return []string{}, nil
+	out := []string{}
+
+	for i := 0; i < int(math.Ceil(float64(len(unsplited))/float64(KB_PER_SPLIT_IN_BYTES))); i++ {
+		str := ""
+
+		start := i * KB_PER_SPLIT_IN_BYTES
+		end := start + KB_PER_SPLIT_IN_BYTES
+		if end > len(unsplited) {
+			end = len(unsplited)
+		}
+
+		for j := start; j < end; j++ {
+			str += string(unsplited[j])
+		}
+		out = append(out, str)
+	}
+
+	return out, nil
 }
