219b5a1c068b8960c42481eabe06398017216f28
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-24 19:36:04 +1000

    stop printing output

--- upload.go
+++ upload.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aixoio/pastestorage/converter"
 )
 
 func UploadFile(filename string) {
-	fmt.Println(converter.ConvertFileToText(filename))
+	converter.ConvertFileToText(filename)
 }
