0b322a0095c114da0726a99bda2b64c1bc229a1d
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-24 17:44:43 +1000

    Add license mode

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
 	ShowLicenseMessage()
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: pastestorage <MODE>")
+		os.Exit(1)
+	}
+
+	if strings.Compare(strings.ToLower(os.Args[1]), "license") == 0 {
+		ShowFullLicense()
+	}
+
 }
