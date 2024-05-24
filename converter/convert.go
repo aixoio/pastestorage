6ecdd0e1637d3a299ec83b24605991363407acb1
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-24 19:31:27 +1000

    make faster

--- converter/convert.go
+++ converter/convert.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"math"
 	"os"
+	"sync"
 )
 
 const KB_PER_SPLIT_IN_BYTES = 512000
@@ -16,22 +17,25 @@ func ConvertFileToText(filename string) ([]string, error) {
 
 	unsplited := base64.StdEncoding.EncodeToString(file_dat)
 
-	out := []string{}
+	split_count := int(math.Ceil(float64(len(unsplited)) / float64(KB_PER_SPLIT_IN_BYTES)))
+	out := make([]string, split_count)
+	var wg sync.WaitGroup
 
-	for i := 0; i < int(math.Ceil(float64(len(unsplited))/float64(KB_PER_SPLIT_IN_BYTES))); i++ {
-		str := ""
+	wg.Add(split_count)
 
-		start := i * KB_PER_SPLIT_IN_BYTES
-		end := start + KB_PER_SPLIT_IN_BYTES
-		if end > len(unsplited) {
-			end = len(unsplited)
-		}
-
-		for j := start; j < end; j++ {
-			str += string(unsplited[j])
-		}
-		out = append(out, str)
+	for i := 0; i < split_count; i++ {
+		go func(i int) {
+			defer wg.Done()
+			start := i * KB_PER_SPLIT_IN_BYTES
+			end := start + KB_PER_SPLIT_IN_BYTES
+			if end > len(unsplited) {
+				end = len(unsplited)
+			}
+			out[i] = unsplited[start:end]
+		}(i)
 	}
+
+	wg.Wait()
 
 	return out, nil
 }
