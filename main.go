b717d5b429191dfce4794eee1661449123646fb7
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-24 17:53:51 +1000

    add empty upload code

--- main.go
+++ main.go
@@ -18,4 +18,13 @@ func main() {
 		return
 	}
 
+	if strings.Compare(strings.ToLower(os.Args[1]), "upload") == 0 {
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: pastestorage upload <FILENAME>")
+			os.Exit(1)
+		}
+		UploadFile(os.Args[2])
+		return
+	}
+
 }
