57909c2df415ef3485023c0ebf906532d07a62a8
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-24 18:20:54 +1000

    read file

--- converter/convert.go
+++ converter/convert.go
@@ -1,8 +1,14 @@
 package converter
 
+import "os"
+
 const KB_PER_SPLIT_IN_BYTES = 512000
 
 func ConvertFileToText(filename string) (string, error) {
+	file_dat, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
 
 	return "", nil
 }
